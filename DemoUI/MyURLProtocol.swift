1e1fc9fa6faa2222305384f9310731e4e2213553
IamAlchemist <Wizard.Lee@qq.com>
2016-08-09 16:38:42 +0800

    minor change

--- DemoUI/MyURLProtocol.swift
+++ DemoUI/MyURLProtocol.swift
@@ -10,10 +10,56 @@ import UIKit
 
 var requestCount = 0
 
-class MyURLProtocol: NSURLProtocol {
+class MyURLProtocol: NSURLProtocol, NSURLConnectionDelegate {
+    
+    var connection: NSURLConnection!
+    
     override class func canInitWithRequest(request: NSURLRequest) -> Bool {
+        if NSURLProtocol.propertyForKey("MyURLProtocolHandledKey", inRequest: request) != nil {
+            return false
+        }
+        
         print("Request #\(requestCount): URL = \(request.URL?.absoluteString ?? "empty url")")
         requestCount += 1
-        return false
-    } 
+        
+        return true
+    }
+    
+    override class func canonicalRequestForRequest(request: NSURLRequest) -> NSURLRequest {
+        return request
+    }
+    
+    override class func requestIsCacheEquivalent(aRequest: NSURLRequest,
+                                                 toRequest bRequest: NSURLRequest) -> Bool {
+        return super.requestIsCacheEquivalent(aRequest, toRequest:bRequest)
+    }
+    
+    override func startLoading() {
+        let newRequest = self.request.mutableCopy() as! NSMutableURLRequest
+        NSURLProtocol.setProperty(true, forKey: "MyURLProtocolHandledKey", inRequest: newRequest)
+        self.connection = NSURLConnection(request: newRequest, delegate: self)
+    }
+    
+    override func stopLoading() {
+        if self.connection != nil {
+            self.connection.cancel()
+        }
+        self.connection = nil
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveResponse response: NSURLResponse!) {
+        self.client!.URLProtocol(self, didReceiveResponse: response, cacheStoragePolicy: .NotAllowed)
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!) {
+        self.client!.URLProtocol(self, didLoadData: data)
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        self.client!.URLProtocolDidFinishLoading(self)
+    }
+    
+    func connection(connection: NSURLConnection, didFailWithError error: NSError) {
+        self.client!.URLProtocol(self, didFailWithError: error)
+    }
 }
