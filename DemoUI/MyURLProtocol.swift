b0962ead04611828604f383305ca7198b703bded
IamAlchemist <Wizard.Lee@qq.com>
2016-08-10 13:21:43 +0800

    replace url connection with url session

--- DemoUI/MyURLProtocol.swift
+++ DemoUI/MyURLProtocol.swift
@@ -12,7 +12,7 @@ var requestCount = 0
 
 class MyURLProtocol: NSURLProtocol, NSURLConnectionDelegate {
     
-    var connection: NSURLConnection!
+    var session: NSURLSession!
     
     override class func canInitWithRequest(request: NSURLRequest) -> Bool {
         if NSURLProtocol.propertyForKey("MyURLProtocolHandledKey", inRequest: request) != nil {
@@ -37,14 +37,16 @@ class MyURLProtocol: NSURLProtocol, NSURLConnectionDelegate {
     override func startLoading() {
         let newRequest = self.request.mutableCopy() as! NSMutableURLRequest
         NSURLProtocol.setProperty(true, forKey: "MyURLProtocolHandledKey", inRequest: newRequest)
-        self.connection = NSURLConnection(request: newRequest, delegate: self)
+        
+        session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration(), delegate: self, delegateQueue: nil)
+        let task = session.dataTaskWithRequest(newRequest)
+        task.resume()
     }
     
     override func stopLoading() {
-        if self.connection != nil {
-            self.connection.cancel()
+        if let session = session {
+            session.invalidateAndCancel()
         }
-        self.connection = nil
     }
     
     func connection(connection: NSURLConnection!, didReceiveResponse response: NSURLResponse!) {
@@ -61,5 +63,44 @@ class MyURLProtocol: NSURLProtocol, NSURLConnectionDelegate {
     
     func connection(connection: NSURLConnection, didFailWithError error: NSError) {
         self.client!.URLProtocol(self, didFailWithError: error)
+    }
+}
+
+extension MyURLProtocol: NSURLSessionTaskDelegate {
+    func URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didBecomeDownloadTask downloadTask: NSURLSessionDownloadTask) {
+        print("download")
+    }
+    
+    func URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didBecomeStreamTask streamTask: NSURLSessionStreamTask) {
+        print("strem")
+    }
+}
+
+extension MyURLProtocol: NSURLSessionDelegate {
+}
+
+extension MyURLProtocol: NSURLSessionDownloadDelegate {
+    func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didFinishDownloadingToURL location: NSURL) {
+        print("download finish")
+    }
+}
+
+extension MyURLProtocol: NSURLSessionDataDelegate {
+    func URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didReceiveResponse response: NSURLResponse, completionHandler: (NSURLSessionResponseDisposition) -> Void) {
+        client?.URLProtocol(self, didReceiveResponse: response, cacheStoragePolicy: .NotAllowed)
+        completionHandler(.Allow)
+    }
+    
+    func URLSession(session: NSURLSession, task: NSURLSessionTask, didCompleteWithError error: NSError?) {
+        if let error = error {
+            client?.URLProtocol(self, didFailWithError: error)
+        }
+        else {
+            client?.URLProtocolDidFinishLoading(self)
+        }
+    }
+    
+    func URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didReceiveData data: NSData) {
+        client?.URLProtocol(self, didLoadData: data)
     }
 }
